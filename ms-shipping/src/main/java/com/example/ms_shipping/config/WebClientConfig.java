8348f188b77aaf076ece326c664d9db66a9d806d
ValeRod-dc <vale.rodrigueza@duocuc.cl>
2026-07-12 13:49:38 -0400

    Se configuran los timeouts

--- ms-shipping/src/main/java/com/example/ms_shipping/config/WebClientConfig.java
+++ ms-shipping/src/main/java/com/example/ms_shipping/config/WebClientConfig.java
@@ -3,7 +3,11 @@ package com.example.ms_shipping.config;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+import java.time.Duration;
 
 @Configuration
 public class WebClientConfig {
@@ -11,6 +15,9 @@ public class WebClientConfig {
     @Bean
     @LoadBalanced
     public WebClient.Builder webClientBuilder() {
-        return WebClient.builder();
+        HttpClient httpClient = HttpClient.create()
+                .responseTimeout(Duration.ofSeconds(5));
+        return WebClient.builder()
+                .clientConnector(new ReactorClientHttpConnector(httpClient));
     }
 }
